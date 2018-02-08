ccb865cd1f8d6a3f4bea58981dc0e49260fc5736
SampaNag <sampa.nag@gmail.com>
2018-02-08 21:45:46 +0000

    Initial commit

---
+++ src/main/java/automationPractice/Utils/AutomationConstants.java
@@ -0,0 +1,19 @@
+package automationPractice.Utils;
+
+/**
+ * Created by User on 19/11/2017.
+ */
+public class AutomationConstants {
+    //Global Test Data
+    public static final String baseURL = "http://automationpractice.com";
+    public static final int TIMEOUT =30;
+    //Browser settings
+    public static final String BROWSER_TYPE = "Chrome";
+
+
+    public static final boolean REMOTE_BROWSER = true;
+//   public static final String SELENIUM_GRID_URL ="http://localhost:4441/wd/hub";
+
+    public static final String SELENIUM_GRID_URL ="http://natprojects21:d07afe81-850f-44d3-a2ed-5807f1c039a6@ondemand.saucelabs.com:80/wd/hub";
+
+}
